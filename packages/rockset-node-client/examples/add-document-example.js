9e328952a3cd69b1c00dd39bff14591df36d3fa7
Kshitij Wadhwa <kw@rockset.com>
2018-10-29 22:30:47 +0000

    example for add documents

---
+++ packages/rockset-node-client/examples/add-document-example.js
@@ -0,0 +1,42 @@
+var apiKey = process.env.ROCKSET_APIKEY;
+var apiServer = process.env.ROCKSET_APISERVER;
+var rockset = require('../src/rockset')(apiKey, apiServer);
+
+
+var getResponseLogger = function (callback) {
+  return function (error, response, body) {
+      if (error) {
+        console.log(error.response.error.text);
+        callback();
+      } else {
+        console.log(response)
+        callback();
+      }
+    }
+}
+
+function example1() {
+  console.log('\n\n=== Create a Collection ===');
+  rockset.collections.create({
+    'name': 'myjscollection'
+  }, getResponseLogger(example2))
+}
+
+
+function example2() {
+  console.log('\n\n=== Add documents to collection ===');
+  rockset.documents.add(
+    'myjscollection',
+    {
+      'data': [{
+        "name": "foo"
+      }]
+  }, getResponseLogger(done))
+}
+
+function done() {
+  console.log('\n\n=== done ===');
+}
+
+
+example1()
