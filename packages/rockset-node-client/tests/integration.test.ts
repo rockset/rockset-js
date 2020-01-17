6983f3af2b315437603f8f07b982363f91a4d11b
Tanmay Chordia <tchordia@gmail.com>
2020-01-17 14:24:59 -0800

    add a unit test for get collections

--- packages/rockset-node-client/tests/integration.test.ts
+++ packages/rockset-node-client/tests/integration.test.ts
@@ -55,6 +55,29 @@ describe("Rockset Unit Tests", function() {
     });
   });
 
+  test("getting collections", async () => {
+    const out = await rockset.collections.getCollection("commons", "_events");
+    const sampleOutput = {
+      data: {
+        created_at: "2019-10-16T01:46:55Z",
+        created_by: null,
+        creation_time: 1571190415924,
+        description: null,
+        field_mappings: [],
+        name: "_events",
+        retention_secs: 2592000,
+        sources: [],
+        stats: out?.data?.stats,
+        status: "READY",
+        workspace: "commons"
+      }
+    };
+
+    expect(Object.getOwnPropertyNames(out)).toMatchObject(
+      Object.getOwnPropertyNames(sampleOutput)
+    );
+  });
+
   test("deleting a collection", async () => {
     try {
       const result = await rockset.collections.deleteCollection(
