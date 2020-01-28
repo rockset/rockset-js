94af0c83403988674da3ef114055111bee53eb74
Scott Morris <scott@rockset.com>
2020-01-28 14:10:14 -0800

    update client and add saved query tests

--- packages/rockset-node-client/tests/axios.test.ts
+++ packages/rockset-node-client/tests/axios.test.ts
@@ -5,7 +5,7 @@ const basePath = process.env.ROCKSET_HOST || "https://api.rs2.usw2.rockset.com";
 const apikey = process.env.ROCKSET_APIKEY as string;
 
 if (apikey == null) {
-  throw "No APIKEY specified. Please specify an environment variable APIKEY with your rockset apikey. eg: $ APIKEY=abc jest";
+  throw "No ROCKSET_APIKEY specified. Please specify an environment variable ROCKSET_APIKEY with your Rockset key. eg: $ export ROCKSET_APIKEY=...";
 }
 
 const customFetchAxios = async (url: string, options: any) => {
@@ -28,6 +28,12 @@ const collection =
     .toString(36)
     .slice(2);
 
+const savedQuery =
+  "test_query_" +
+  Math.random()
+    .toString(36)
+    .slice(2);
+
 afterAll(function() {});
 describe("Rockset Unit Tests", function() {
   test("creating a collection", async () => {
@@ -39,7 +45,6 @@ describe("Rockset Unit Tests", function() {
         data: {
           created_at: null,
           created_by: null,
-          creation_time: null,
           description: null,
           field_mappings: [],
           name: collection,
@@ -64,7 +69,7 @@ describe("Rockset Unit Tests", function() {
     expect(out).toMatchObject({
       collections: ["commons._events"],
       column_fields: [{ name: "?count", type: "" }],
-      results: [{ "?count": out?.results?.[0]?.["?count"] }],
+      results: [{ "?count": expect.anything() }],
       stats: { rows_scanned: 0 }
     });
   });
@@ -79,7 +84,6 @@ describe("Rockset Unit Tests", function() {
         data: {
           created_at: null,
           created_by: null,
-          creation_time: null,
           description: null,
           field_mappings: [],
           name: collection,
@@ -94,4 +98,99 @@ describe("Rockset Unit Tests", function() {
       fail(e);
     }
   });
+
+  test("creating a saved query", async () => {
+    try {
+      const result = await rockset.queries.createSavedQuery("commons", {
+        name: savedQuery,
+        query_sql: 'SELECT :param as echo',
+        parameters: [{
+          name: 'param',
+          type: 'string',
+          default_value: 'Hello world!'
+        }]
+      });
+      expect(result).toMatchObject({
+        data: {
+          created_at: expect.anything(),
+          created_by: expect.anything(),
+          name: savedQuery,
+          workspace: "commons",
+          version: 1,
+          version_tag: null,
+          query_sql: 'SELECT :param as echo',
+          parameters: [{
+            name: 'param',
+            type: 'string',
+            default_value: 'Hello world!'
+          }],
+          stats: expect.anything(),
+          collections: [],
+        }
+      });
+    } catch (e) {
+      fail(e);
+    }
+  });
+
+  test("running a saved query with default parameters", async () => {
+    try {
+      const result = await rockset.queries.runSavedQuery("commons", savedQuery, 1);
+      expect(result).toMatchObject({
+        results: [{
+          "echo": "Hello world!",
+        }],
+        stats: expect.anything(),
+      });
+    } catch (e) {
+      fail(e);
+    }
+  });
+
+  test("running a saved query with custom parameters", async () => {
+    try {
+      const result = await rockset.queries.runSavedQuery("commons", savedQuery, 1, {
+        parameters: [{
+          name: 'param',
+          value: 'All work and no play makes Jack a dull boy',
+        }]
+      });
+      expect(result).toMatchObject({
+        results: [{
+          "echo": "All work and no play makes Jack a dull boy",
+        }],
+        stats: expect.anything(),
+      });
+    } catch (e) {
+      fail(e);
+    }
+  });
+
+  test("deleting a saved query", async () => {
+    try {
+      const result = await rockset.queries.deleteSavedQuery("commons", savedQuery);
+      expect(result).toMatchObject({
+        data: {
+          created_at: expect.anything(),
+          created_by: expect.anything(),
+          name: savedQuery,
+          workspace: "commons",
+          version: 1,
+          version_tag: null,
+          query_sql: 'SELECT :param as echo',
+          parameters: [{
+            name: 'param',
+            type: 'string',
+            default_value: 'Hello world!'
+          }],
+          stats: expect.anything(),
+          collections: [],
+        }
+      });
+    } catch (e) {
+      fail(e);
+    }
+  });
+
+
 });
--- packages/rockset-node-client/tests/integration.test.ts
+++ packages/rockset-node-client/tests/integration.test.ts
@@ -4,7 +4,7 @@ const basePath = process.env.ROCKSET_HOST || "https://api.rs2.usw2.rockset.com";
 const apikey = process.env.ROCKSET_APIKEY as string;
 
 if (apikey == null) {
-  throw "No APIKEY specified. Please specify an environment variable APIKEY with your rockset apikey. eg: $ APIKEY=abc jest";
+  throw "No ROCKSET_APIKEY specified. Please specify an environment variable ROCKSET_APIKEY with your Rockset key. eg: $ export ROCKSET_APIKEY=...";
 }
 
 const rockset = rocksetConfigure(apikey, basePath);
@@ -14,6 +14,12 @@ const collection =
     .toString(36)
     .slice(2);
 
+const savedQuery =
+  "test_query_" +
+  Math.random()
+    .toString(36)
+    .slice(2);
+  
 afterAll(function() {});
 describe("Rockset Unit Tests", function() {
   test("creating a collection", async () => {
@@ -25,7 +31,6 @@ describe("Rockset Unit Tests", function() {
         data: {
           created_at: null,
           created_by: null,
-          creation_time: null,
           description: null,
           field_mappings: [],
           name: collection,
@@ -50,7 +55,7 @@ describe("Rockset Unit Tests", function() {
     expect(out).toMatchObject({
       collections: ["commons._events"],
       column_fields: [{ name: "?count", type: "" }],
-      results: [{ "?count": out?.results?.[0]?.["?count"] }],
+      results: [{ "?count": expect.anything() }],
       stats: { rows_scanned: 0 }
     });
   });
@@ -61,13 +66,12 @@ describe("Rockset Unit Tests", function() {
       data: {
         created_at: "2019-10-16T01:46:55Z",
         created_by: null,
-        creation_time: 1571190415924,
         description: null,
         field_mappings: [],
         name: "_events",
         retention_secs: 2592000,
         sources: [],
-        stats: out?.data?.stats,
+        stats: expect.anything(),
         status: "READY",
         workspace: "commons"
       }
@@ -88,7 +92,6 @@ describe("Rockset Unit Tests", function() {
         data: {
           created_at: null,
           created_by: null,
-          creation_time: null,
           description: null,
           field_mappings: [],
           name: collection,
@@ -103,4 +106,99 @@ describe("Rockset Unit Tests", function() {
       fail(e);
     }
   });
+
+  test("creating a saved query", async () => {
+    try {
+      const result = await rockset.queries.createSavedQuery("commons", {
+        name: savedQuery,
+        query_sql: 'SELECT :param as echo',
+        parameters: [{
+          name: 'param',
+          type: 'string',
+          default_value: 'Hello world!'
+        }]
+      });
+      expect(result).toMatchObject({
+        data: {
+          created_at: expect.anything(),
+          created_by: expect.anything(),
+          name: savedQuery,
+          workspace: "commons",
+          version: 1,
+          version_tag: null,
+          query_sql: 'SELECT :param as echo',
+          parameters: [{
+            name: 'param',
+            type: 'string',
+            default_value: 'Hello world!'
+          }],
+          stats: expect.anything(),
+          collections: [],
+        }
+      });
+    } catch (e) {
+      fail(e);
+    }
+  });
+
+  test("running a saved query with default parameters", async () => {
+    try {
+      const result = await rockset.queries.runSavedQuery("commons", savedQuery, 1);
+      expect(result).toMatchObject({
+        results: [{
+          "echo": "Hello world!",
+        }],
+        stats: expect.anything(),
+      });
+    } catch (e) {
+      fail(e);
+    }
+  });
+
+  test("running a saved query with custom parameters", async () => {
+    try {
+      const result = await rockset.queries.runSavedQuery("commons", savedQuery, 1, {
+        parameters: [{
+          name: 'param',
+          value: 'All work and no play makes Jack a dull boy',
+        }]
+      });
+      expect(result).toMatchObject({
+        results: [{
+          "echo": "All work and no play makes Jack a dull boy",
+        }],
+        stats: expect.anything(),
+      });
+    } catch (e) {
+      fail(e);
+    }
+  });
+
+  test("deleting a saved query", async () => {
+    try {
+      const result = await rockset.queries.deleteSavedQuery("commons", savedQuery);
+      expect(result).toMatchObject({
+        data: {
+          created_at: expect.anything(),
+          created_by: expect.anything(),
+          name: savedQuery,
+          workspace: "commons",
+          version: 1,
+          version_tag: null,
+          query_sql: 'SELECT :param as echo',
+          parameters: [{
+            name: 'param',
+            type: 'string',
+            default_value: 'Hello world!'
+          }],
+          stats: expect.anything(),
+          collections: [],
+        }
+      });
+    } catch (e) {
+      fail(e);
+    }
+  });
+
+
 });
