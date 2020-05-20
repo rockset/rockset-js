cbfb1e877b7c85f417d31e91f734ea8e1f57a185
Scott Morris <scott@rockset.com>
2020-05-19 17:36:51 -0700

    fix build

--- packages/client/examples/custom-fetch-axios.ts
+++ packages/client/examples/custom-fetch-axios.ts
@@ -5,7 +5,7 @@
  * Note: this method DOES NOT cancel the request on rockset servers. It simply rejects the promise returned by axios.
  */
 import axios from 'axios';
-import rocksetConfigure from 'rockset';
+import rocksetConfigure from '../src';
 
 // Super simple fetch with axios: axios docs show how to check for errors, cancel requests etc.
 const customFetchAxios = async (
