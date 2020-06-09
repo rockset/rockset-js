299202ad79bc6ed179921ad26efc6e7e143ce19e
Tanmay Chordia <tchordia@gmail.com>
2020-06-09 13:42:30 -0700

    add secrets to env to fix the release process

---
+++ test.sh
@@ -0,0 +1,8 @@
+while true; do
+    read -p "Do you wish to install this program?" yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
