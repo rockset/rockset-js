2ec8d0ab4a0c31ec9c8baed16b0632dad2e46e71
Tanmay Chordia <tchordia@gmail.com>
2020-06-26 15:15:11 -0700

    add support for rock sql
    Summary: 1. Add support for rock sql
    
    Test Plan:
    1. `rock sql` opens an editor to write sql
    1. `rock sql "select 6"` runs a query
    1. Table flags allow you to select a column
    
    a little bit hard to do a screencap of cli, but here:
    
    {F46197}
    
    Reviewers: #product, scott
    
    Reviewed By: #product, scott
    
    Subscribers: scott
    
    Differential Revision: https://rockset.phacility.com/D7155

---
+++ packages/cli/src/commands/sql.ts
@@ -0,0 +1,75 @@
+// Generated file, please do not edit directly
+import { flags } from '@oclif/command';
+import { RockCommand } from '../base-command';
+import { main } from '@rockset/core';
+import * as _ from 'lodash';
+import * as inquirer from 'inquirer';
+import { cli } from 'cli-ux';
+
+class SQL extends RockCommand {
+  static flags = {
+    help: flags.help({ char: 'h' }),
+    defaultLimit: flags.integer({
+      char: 'l',
+      description:
+        'Set the default row limit property on the query. This will set a default limit on the query that can be overrided by a limit in the SQL text.',
+    }),
+    ...cli.table.flags(),
+  };
+
+  static args = [
+    {
+      name: 'sql',
+      description: 'Rockset SQL string to execute',
+      required: false,
+      hidden: false,
+    },
+  ];
+
+  static examples = [
+    `
+  // Runs a sql query passed as a string
+  rock sql QUERY
+
+  // Opens your default editor to edit more complex queries
+  rock sql -e
+  `,
+  ];
+
+  static description = `
+Execute a SQL expression.
+`;
+
+  async run() {
+    const { args, flags } = this.parse(SQL);
+
+    const sql = args.sql as string;
+
+    const execute = async (sql: string) => {
+      const client = await main.createClient();
+      if (sql && _.isString(sql)) {
+        const data = await client.queries.query({
+          sql: {
+            query: sql,
+            default_row_limit: flags.defaultLimit,
+          },
+        });
+        const unwrapData = data?.results ?? [];
+        const columns = Object.getOwnPropertyNames(unwrapData?.[0]);
+        const col = columns.reduce((obj, cur) => ({ ...obj, [cur]: {} }), {});
+
+        cli.table(unwrapData, col, { ...flags });
+      }
+    };
+
+    if (sql) {
+      await execute(sql);
+    } else {
+      const type = 'editor';
+      const sql = await inquirer.prompt([{ name: 'sql', type }]);
+      await execute(sql.sql);
+    }
+  }
+}
+
+export default SQL;
