126303be0d57b407a0ea175c26f93658a0ba7dc7
Tanmay Chordia <tchordia@gmail.com>
2020-07-08 11:11:33 -0700

    remove lib from gitignore
    Summary: 1. Remove lib from gitignore
    
    Test Plan: 1. Add lib files to project
    
    Reviewers: #product, brian
    
    Reviewed By: #product, brian
    
    Differential Revision: https://rockset.phacility.com/D7296

---
+++ packages/dev-ui/src/lib/constants/distinctColors.ts
@@ -0,0 +1,601 @@
+export const distinctColors: { name: string; rgba: number[]; hex: string }[] = [
+  {
+    name: 'dimgray',
+    rgba: [105, 105, 105],
+    hex: '#696969',
+  },
+
+  {
+    name: 'gray',
+    rgba: [128, 128, 128],
+    hex: '#808080',
+  },
+
+  {
+    name: 'darkgray',
+    rgba: [169, 169, 169],
+    hex: '#a9a9a9',
+  },
+
+  {
+    name: 'silver',
+    rgba: [192, 192, 192],
+    hex: '#c0c0c0',
+  },
+
+  {
+    name: 'darkslategray',
+    rgba: [47, 79, 79],
+    hex: '#2f4f4f',
+  },
+
+  {
+    name: 'darkolivegreen',
+    rgba: [85, 107, 47],
+    hex: '#556b2f',
+  },
+
+  {
+    name: 'saddlebrown',
+    rgba: [139, 69, 19],
+    hex: '#8b4513',
+  },
+
+  {
+    name: 'olivedrab',
+    rgba: [107, 142, 35],
+    hex: '#6b8e23',
+  },
+
+  {
+    name: 'sienna',
+    rgba: [160, 82, 45],
+    hex: '#a0522d',
+  },
+
+  {
+    name: 'brown',
+    rgba: [165, 42, 42],
+    hex: '#a52a2a',
+  },
+
+  {
+    name: 'seagreen',
+    rgba: [46, 139, 87],
+    hex: '#2e8b57',
+  },
+
+  {
+    name: 'forestgreen',
+    rgba: [34, 139, 34],
+    hex: '#228b22',
+  },
+
+  {
+    name: 'maroon2',
+    rgba: [127, 0, 0],
+    hex: '#7f0000',
+  },
+
+  {
+    name: 'midnightblue',
+    rgba: [25, 25, 112],
+    hex: '#191970',
+  },
+
+  {
+    name: 'darkred',
+    rgba: [139, 0, 0],
+    hex: '#8b0000',
+  },
+
+  {
+    name: 'olive',
+    rgba: [128, 128, 0],
+    hex: '#808000',
+  },
+
+  {
+    name: 'darkslateblue',
+    rgba: [72, 61, 139],
+    hex: '#483d8b',
+  },
+
+  {
+    name: 'firebrick',
+    rgba: [178, 34, 34],
+    hex: '#b22222',
+  },
+
+  {
+    name: 'cadetblue',
+    rgba: [95, 158, 160],
+    hex: '#5f9ea0',
+  },
+
+  {
+    name: 'green',
+    rgba: [0, 128, 0],
+    hex: '#008000',
+  },
+
+  {
+    name: 'lightslategray',
+    rgba: [119, 136, 153],
+    hex: '#778899',
+  },
+
+  {
+    name: 'mediumseagreen',
+    rgba: [60, 179, 113],
+    hex: '#3cb371',
+  },
+
+  {
+    name: 'rosybrown',
+    rgba: [188, 143, 143],
+    hex: '#bc8f8f',
+  },
+
+  {
+    name: 'rebeccapurple',
+    rgba: [102, 51, 153],
+    hex: '#663399',
+  },
+
+  {
+    name: 'teal',
+    rgba: [0, 128, 128],
+    hex: '#008080',
+  },
+
+  {
+    name: 'darkgoldenrod',
+    rgba: [184, 134, 11],
+    hex: '#b8860b',
+  },
+
+  {
+    name: 'darkkhaki',
+    rgba: [189, 183, 107],
+    hex: '#bdb76b',
+  },
+
+  {
+    name: 'peru',
+    rgba: [205, 133, 63],
+    hex: '#cd853f',
+  },
+
+  {
+    name: 'steelblue',
+    rgba: [70, 130, 180],
+    hex: '#4682b4',
+  },
+
+  {
+    name: 'chocolate',
+    rgba: [210, 105, 30],
+    hex: '#d2691e',
+  },
+
+  {
+    name: 'yellowgreen',
+    rgba: [154, 205, 50],
+    hex: '#9acd32',
+  },
+
+  {
+    name: 'lightseagreen',
+    rgba: [32, 178, 170],
+    hex: '#20b2aa',
+  },
+
+  {
+    name: 'indianred',
+    rgba: [205, 92, 92],
+    hex: '#cd5c5c',
+  },
+
+  {
+    name: 'darkblue',
+    rgba: [0, 0, 139],
+    hex: '#00008b',
+  },
+
+  {
+    name: 'indigo',
+    rgba: [75, 0, 130],
+    hex: '#4b0082',
+  },
+
+  {
+    name: 'limegreen',
+    rgba: [50, 205, 50],
+    hex: '#32cd32',
+  },
+
+  {
+    name: 'goldenrod',
+    rgba: [218, 165, 32],
+    hex: '#daa520',
+  },
+
+  {
+    name: 'red',
+    rgba: [255, 0, 0],
+    hex: '#ff0000',
+  },
+
+  {
+    name: 'darkseagreen',
+    rgba: [143, 188, 143],
+    hex: '#8fbc8f',
+  },
+
+  {
+    name: 'darkmagenta',
+    rgba: [139, 0, 139],
+    hex: '#8b008b',
+  },
+
+  {
+    name: 'maroon3',
+    rgba: [176, 48, 96],
+    hex: '#b03060',
+  },
+
+  {
+    name: 'tan',
+    rgba: [210, 180, 140],
+    hex: '#d2b48c',
+  },
+
+  {
+    name: 'mediumturquoise',
+    rgba: [72, 209, 204],
+    hex: '#48d1cc',
+  },
+
+  {
+    name: 'mediumaquamarine',
+    rgba: [102, 205, 170],
+    hex: '#66cdaa',
+  },
+
+  {
+    name: 'darkorchid',
+    rgba: [153, 50, 204],
+    hex: '#9932cc',
+  },
+
+  {
+    name: 'purple2',
+    rgba: [127, 0, 127],
+    hex: '#7f007f',
+  },
+
+  {
+    name: 'orangered',
+    rgba: [255, 69, 0],
+    hex: '#ff4500',
+  },
+
+  {
+    name: 'darkturquoise',
+    rgba: [0, 206, 209],
+    hex: '#00ced1',
+  },
+
+  {
+    name: 'darkorange',
+    rgba: [255, 140, 0],
+    hex: '#ff8c00',
+  },
+
+  {
+    name: 'orange',
+    rgba: [255, 165, 0],
+    hex: '#ffa500',
+  },
+
+  {
+    name: 'gold',
+    rgba: [255, 215, 0],
+    hex: '#ffd700',
+  },
+
+  {
+    name: 'slateblue',
+    rgba: [106, 90, 205],
+    hex: '#6a5acd',
+  },
+
+  {
+    name: 'yellow',
+    rgba: [255, 255, 0],
+    hex: '#ffff00',
+  },
+
+  {
+    name: 'mediumvioletred',
+    rgba: [199, 21, 133],
+    hex: '#c71585',
+  },
+
+  {
+    name: 'mediumblue',
+    rgba: [0, 0, 205],
+    hex: '#0000cd',
+  },
+
+  {
+    name: 'lawngreen',
+    rgba: [124, 252, 0],
+    hex: '#7cfc00',
+  },
+
+  {
+    name: 'burlywood',
+    rgba: [222, 184, 135],
+    hex: '#deb887',
+  },
+
+  {
+    name: 'turquoise',
+    rgba: [64, 224, 208],
+    hex: '#40e0d0',
+  },
+
+  {
+    name: 'lime',
+    rgba: [0, 255, 0],
+    hex: '#00ff00',
+  },
+
+  {
+    name: 'darkviolet',
+    rgba: [148, 0, 211],
+    hex: '#9400d3',
+  },
+
+  {
+    name: 'mediumorchid',
+    rgba: [186, 85, 211],
+    hex: '#ba55d3',
+  },
+
+  {
+    name: 'mediumspringgreen',
+    rgba: [0, 250, 154],
+    hex: '#00fa9a',
+  },
+
+  {
+    name: 'blueviolet',
+    rgba: [138, 43, 226],
+    hex: '#8a2be2',
+  },
+
+  {
+    name: 'springgreen',
+    rgba: [0, 255, 127],
+    hex: '#00ff7f',
+  },
+
+  {
+    name: 'royalblue',
+    rgba: [65, 105, 225],
+    hex: '#4169e1',
+  },
+
+  {
+    name: 'darksalmon',
+    rgba: [233, 150, 122],
+    hex: '#e9967a',
+  },
+
+  {
+    name: 'crimson',
+    rgba: [220, 20, 60],
+    hex: '#dc143c',
+  },
+
+  {
+    name: 'aqua',
+    rgba: [0, 255, 255],
+    hex: '#00ffff',
+  },
+
+  {
+    name: 'deepskyblue',
+    rgba: [0, 191, 255],
+    hex: '#00bfff',
+  },
+
+  {
+    name: 'sandybrown',
+    rgba: [244, 164, 96],
+    hex: '#f4a460',
+  },
+
+  {
+    name: 'mediumpurple',
+    rgba: [147, 112, 219],
+    hex: '#9370db',
+  },
+
+  {
+    name: 'blue',
+    rgba: [0, 0, 255],
+    hex: '#0000ff',
+  },
+
+  {
+    name: 'purple3',
+    rgba: [160, 32, 240],
+    hex: '#a020f0',
+  },
+
+  {
+    name: 'lightcoral',
+    rgba: [240, 128, 128],
+    hex: '#f08080',
+  },
+
+  {
+    name: 'greenyellow',
+    rgba: [173, 255, 47],
+    hex: '#adff2f',
+  },
+
+  {
+    name: 'tomato',
+    rgba: [255, 99, 71],
+    hex: '#ff6347',
+  },
+
+  {
+    name: 'orchid',
+    rgba: [218, 112, 214],
+    hex: '#da70d6',
+  },
+
+  {
+    name: 'thistle',
+    rgba: [216, 191, 216],
+    hex: '#d8bfd8',
+  },
+
+  {
+    name: 'lightsteelblue',
+    rgba: [176, 196, 222],
+    hex: '#b0c4de',
+  },
+
+  {
+    name: 'coral',
+    rgba: [255, 127, 80],
+    hex: '#ff7f50',
+  },
+
+  {
+    name: 'fuchsia',
+    rgba: [255, 0, 255],
+    hex: '#ff00ff',
+  },
+
+  {
+    name: 'dodgerblue',
+    rgba: [30, 144, 255],
+    hex: '#1e90ff',
+  },
+
+  {
+    name: 'palevioletred',
+    rgba: [219, 112, 147],
+    hex: '#db7093',
+  },
+
+  {
+    name: 'khaki',
+    rgba: [240, 230, 140],
+    hex: '#f0e68c',
+  },
+
+  {
+    name: 'salmon',
+    rgba: [250, 128, 114],
+    hex: '#fa8072',
+  },
+
+  {
+    name: 'palegoldenrod',
+    rgba: [238, 232, 170],
+    hex: '#eee8aa',
+  },
+
+  {
+    name: 'laserlemon',
+    rgba: [255, 255, 84],
+    hex: '#ffff54',
+  },
+
+  {
+    name: 'cornflower',
+    rgba: [100, 149, 237],
+    hex: '#6495ed',
+  },
+
+  {
+    name: 'plum',
+    rgba: [221, 160, 221],
+    hex: '#dda0dd',
+  },
+
+  {
+    name: 'powderblue',
+    rgba: [176, 224, 230],
+    hex: '#b0e0e6',
+  },
+
+  {
+    name: 'lightblue',
+    rgba: [173, 216, 230],
+    hex: '#add8e6',
+  },
+
+  {
+    name: 'skyblue',
+    rgba: [135, 206, 235],
+    hex: '#87ceeb',
+  },
+
+  {
+    name: 'deeppink',
+    rgba: [255, 20, 147],
+    hex: '#ff1493',
+  },
+
+  {
+    name: 'mediumslateblue',
+    rgba: [123, 104, 238],
+    hex: '#7b68ee',
+  },
+
+  {
+    name: 'lightsalmon',
+    rgba: [255, 160, 122],
+    hex: '#ffa07a',
+  },
+
+  {
+    name: 'violet',
+    rgba: [238, 130, 238],
+    hex: '#ee82ee',
+  },
+
+  {
+    name: 'palegreen',
+    rgba: [152, 251, 152],
+    hex: '#98fb98',
+  },
+
+  {
+    name: 'lightskyblue',
+    rgba: [135, 206, 250],
+    hex: '#87cefa',
+  },
+
+  {
+    name: 'aquamarine',
+    rgba: [127, 255, 212],
+    hex: '#7fffd4',
+  },
+
+  {
+    name: 'hotpink',
+    rgba: [255, 105, 180],
+    hex: '#ff69b4',
+  },
+];
