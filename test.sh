f799b1d5de405c20a7cbf7b11f2c2bdbd9e60ed6
Tanmay Chordia <tchordia@gmail.com>
2020-07-13 15:41:24 -0700

    remove redundant file
    Summary: 1. remove file
    
    Test Plan: 1. ensure build passes
    
    Reviewers: #product, scott
    
    Reviewed By: #product, scott
    
    Differential Revision: https://rockset.phacility.com/D7308

--- test.sh
+++
@@ -1,8 +0,0 @@
-while true; do
-    read -p "Do you wish to install this program? " yn
-    case $yn in
-        [Yy]* ) make install; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
