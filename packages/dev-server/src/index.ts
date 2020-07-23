6cf6cd254c29ecd3f674337d2866005523baa440
Tanmay Chordia <tchordia@gmail.com>
2020-07-23 11:44:25 -0700

    fix serving
    Summary: 1. Fix serving logic
    
    Test Plan: 1. `rockset local:serve` loads the dev-ui page
    
    Reviewers: #product, brian
    
    Reviewed By: #product, brian
    
    Differential Revision: https://rockset.phacility.com/D7552

--- packages/dev-server/src/index.ts
+++ packages/dev-server/src/index.ts
@@ -105,13 +105,12 @@ export async function serve(port = 3001) {
     }
   });
 
-  // This turns off the 304 cache policy
-  app.disable('etag');
+  app.use(express.static(path.join(__dirname, 'ui')));
+
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
   });
 
-  app.use(express.static(path.join(__dirname, 'ui')));
   app.listen(port, () =>
     console.log(`QL dev server listening at http://localhost:${port}`)
   );
