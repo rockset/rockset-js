f570256c7c7ae90ea2b8dd28572497777e75b975
Scott Morris <scott@rockset.com>
2020-10-30 15:02:54 -0700

    add keepalive client example
    Summary: add example to Node.js client source code that uses shared connection when possible.
    
    Test Plan:
    tested against dev and ensured that unlike the vanilla client, the custom client does not result in a spike in new connection in our ELB.
    
    See receivers processing data in both runs here:
    
    {F53115}
    
    But only the first (using vanilla client) results in an ELB spike:
    
    {F53116}
    
    Reviewers: #product, dhruba, veeve
    
    Differential Revision: https://rockset.phacility.com/D8919

---
+++ packages/client/src/examples/custom-fetch-keepalive.ts
@@ -0,0 +1,54 @@
+/**
+ * This file shows how to integrate custom fetch functionality into the rockset fetch api.
+ * We use axios in this example, and re-use shared HTTP connections instead of creating new
+ * connections on every request. Useful for conserving connections for high-throughput actions,
+ * such as writing documents to collections at a high rate.
+ */
+
+import rockset from "@rockset/client";
+import axios from "axios";
+import * as HttpAgent from "agentkeepalive";
+
+// Configure
+const basePath = process.env.ROCKSET_HOST || 'https://api.rs2.usw2.rockset.com';
+const apikey = process.env.ROCKSET_APIKEY as string;
+
+const { HttpsAgent } = HttpAgent;
+
+// create axios instance with agentkeepalive -- re-use shared session when 
+// possible instead of creating new sessions on each request.
+const axiosInstance = axios.create({
+  httpAgent:  new HttpAgent(),
+  httpsAgent:  new HttpsAgent()
+});
+
+const customFetchKeepAlive = async (
+  url: string,
+  { headers, method, body: data, queryParams: params }: any
+) => {
+  const res = await axiosInstance.request({
+    url,
+    headers,
+    method,
+    data,
+    params,
+  });
+  return res.data;
+};
+
+const rocksetClient = rockset(apikey, basePath, customFetchKeepAlive);
+
+
+const writeData = async (data: any) => {
+  const res = await rocksetClient.documents.addDocuments("commons", "asdfasdf", {
+    data
+  }).catch(err => {
+    console.log(err.code)
+  });
+  return res;
+}
+
+// call write from your application:
+// ...
+// writeData([{ 'some': 'data' }]);
+// ...
