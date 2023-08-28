12989fa2a66b0eb4e108677846a8f43c8639d08d
Julius Hochmuth <julius@rockset.com>
2023-08-28 13:22:12 -0600

    Check existing versions before deploy.

--- packages/core/src/tests/main.test.ts
+++ packages/core/src/tests/main.test.ts
@@ -33,4 +33,70 @@ describe('Main external facing functions', () => {
       ]);
     })
   );
+
+  test(
+    'Test deploy',
+    mfs('emptyRoot', async () => {
+      // Write a lambda to commons.foo, and expect to see it listed
+      const willSkip = createEmptyQLEntity(
+        parseLambdaQualifiedName('commons.willSkip'),
+        'blah'
+      );
+      willSkip.sql = 'SELECT 1';
+      const willDeploy = createEmptyQLEntity(
+        parseLambdaQualifiedName('commons.willDeploy'),
+        'blah'
+      );
+      willDeploy.sql = 'SELECT 2';
+
+      await Promise.all([writeLambda(willSkip), writeLambda(willDeploy)]);
+
+      const startHook = jest.fn();
+      const skipHook = jest.fn();
+      const successHook = jest.fn();
+
+      const mockClient = {
+        workspaces: { listWorkspaces: jest.fn(() => ['commons']) },
+        queryLambdas: {
+          listQueryLambdaVersions: jest.fn(() => ({
+            data: [
+              {
+                sql: { query: 'SELECT 1', default_parameters: [] },
+                description: 'blah',
+              },
+            ],
+          })),
+          updateQueryLambda: jest.fn((_, lambda) => {
+            if (lambda === 'willDeploy') {
+              return { version: 'willDeployVersion' };
+            }
+            return { version: 'willSkipVersion' };
+          }),
+        },
+      };
+
+      await main.deployQueryLambdas(
+        {
+          onDeployStart: startHook,
+          onSkipQueryLambda: skipHook,
+          onDeployVersionSuccess: successHook,
+        },
+        {},
+        // @ts-ignore
+        mockClient
+      );
+
+      expect(startHook).toHaveBeenCalledTimes(2);
+
+      expect(skipHook).toHaveBeenCalledTimes(1);
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      expect(skipHook.mock.calls?.[0]?.[0]).toBe('commons.willSkip');
+
+      expect(successHook).toHaveBeenCalledTimes(1);
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      expect(successHook.mock.calls?.[0]?.[0]?.version).toBe(
+        'willDeployVersion'
+      );
+    })
+  );
 });
